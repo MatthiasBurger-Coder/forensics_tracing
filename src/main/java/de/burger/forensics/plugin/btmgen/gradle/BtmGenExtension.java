4404e85ffe629344fe4b171004a7032d603c841a
Matthias Burger <github@family-burger.de>
2025-11-30 17:22:31 +0100

    Annotate BtmGenExtension fields with Lombok Getter and Setter annotations.

--- src/main/java/de/burger/forensics/plugin/btmgen/gradle/BtmGenExtension.java
+++ src/main/java/de/burger/forensics/plugin/btmgen/gradle/BtmGenExtension.java
@@ -3,6 +3,8 @@ package de.burger.forensics.plugin.btmgen.gradle;
 import de.burger.forensics.plugin.btmgen.render.api.RuleParams;
 import de.burger.forensics.plugin.btmgen.render.spi.StrategyRegistries;
 import de.burger.forensics.plugin.btmgen.render.spi.StrategyRegistry;
+import lombok.Getter;
+import lombok.Setter;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
 import org.jetbrains.annotations.NotNull;
@@ -13,13 +15,31 @@ import java.io.File;
 /**
  * Extension to configure sourceRoot, outputFile and provide a StrategyRegistry.
  */
+@Getter
 public abstract class BtmGenExtension {
 
+    /**
+     * -- GETTER --
+     * Strategy registry used by tasks to render rules.
+     */
+    @Setter
     private StrategyRegistry registry = StrategyRegistries.defaultRegistry();
+    /**
+     * -- GETTER --
+     * Where to scan sources (root folder).
+     */
     private final Property<@NotNull File> sourceRoot;
+    /**
+     * -- GETTER --
+     * Output .btm file location.
+     */
     private final Property<@NotNull File> outputFile;
     private final Property<@NotNull String> includes;
     private final Property<@NotNull File> sourceRootFile;
+    /**
+     * -- GETTER --
+     * Fully qualified helper class invoked from generated rules.
+     */
     private final Property<@NotNull String> helperFqn;
 
     @Inject
@@ -34,20 +54,4 @@ public abstract class BtmGenExtension {
         this.helperFqn.convention(RuleParams.DEFAULT_HELPER_FQN);
     }
 
-    /** Strategy registry used by tasks to render rules. */
-    public StrategyRegistry getRegistry() { return registry; }
-    public void setRegistry(StrategyRegistry registry) { this.registry = registry; }
-
-    /** Where to scan sources (root folder). */
-    public Property<@NotNull File> getSourceRoot() { return sourceRoot; }
-
-    /** Output .btm file location. */
-    public Property<@NotNull File> getOutputFile() { return outputFile; }
-
-    public Property<@NotNull String> getIncludes() { return includes; }
-
-    public Property<@NotNull File> getSourceRootFile() { return sourceRootFile; }
-
-    /** Fully qualified helper class invoked from generated rules. */
-    public Property<@NotNull String> getHelperFqn() { return helperFqn; }
 }
